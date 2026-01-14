ff019be489bdb87d323f358ac104ddb9bc325140
Prachit <prachithonrao45@gmail.com>
2026-01-15 00:19:20 +0530

    Add vercel config for vercel deployment

--- Backend/server.js
+++ Backend/server.js
@@ -13,5 +13,8 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+module.exports = app;
